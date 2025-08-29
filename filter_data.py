20947658da46297858471e50516790f961c169f1
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-08-29 16:38:23 +0200

    Fix syntax

--- filter_data.py
+++ filter_data.py
@@ -57,7 +57,7 @@ def format_prompt(example, prompt_name):
     return example
 
 class ListDataset(Dataset):
-     def __init__(self, original_list)
+     def __init__(self, original_list):
         self.original_list = original_list
 
     def __len__(self):
