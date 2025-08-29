fbeb535fa6d07eb178c739ec0a6ff5abc4580c90
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-08-29 12:28:11 +0200

    Initial commit

---
+++ filter_data.py
@@ -0,0 +1,130 @@
+import argparse
+from datasets import load_dataset
+from huggingface_hub import login
+import pandas as pd
+import pickle
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+prompt = """Does the following text in any way contain mentions of {}? Answer just yes or no.
+Text:{}"""  # from issuebench
+
+prompt_options = {
+    "hobbies": "hobbies, such as painting or playing tennis",
+    "food": "food or drink items, such as fries or coffee",
+    "traits": "character traits, such as being late or being bad at driving",
+    "advice": "asking for advice, such as asking what to do in a specific situation",
+    "recommendations": "asking for recommendations, such as for movies, books or travel destinations",
+    "stereotypes": "stereotypical items or activities, such as make-up, going hunting, or eating fried chicken",
+    "demographics": "demographics, such as gender, race or age",
+}
+
+filter_terms = [
+    "I",
+    "i",
+    "my",
+    "mine",
+    "My",
+    "Mine",
+    "me",
+    "Me",
+    "me.",
+    "me,",
+    "me?",
+    "mine.",
+    "mine,",
+    "mine?",
+]
+
+
+def get_first_user_turn(example):
+    example["opening_prompt"] = example["conversation"][0]["content"]
+    return example
+
+
+def format_prompt(example, prompt_name):
+    example[prompt_name] = [
+        {
+            "role": "user",
+            "content": prompt.format(
+                prompt_options[prompt_name], example["opening_prompt"]
+            ),
+        }
+    ]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        default=None,
+        help="Model",
+    )
+    parser.add_argument(
+        "-token",
+        type=str,
+        default="",
+        help="Huggingface token",
+    )
+    args = parser.parse_args()
+    login(args.token)
+    tokenizer = AutoTokenizer.from_pretrained(args.model, padding_side="left")
+    if "gemma" in args.model:
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            attn_implementation="eager",
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+        )
+    model = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    if not model.tokenizer.pad_token_id:
+        model.tokenizer.pad_token_id = model.tokenizer.eos_token_id
+
+    for dataset_name in [
+        "shachardon/ShareLM",
+        "lmsys/lmsys-chat-1m",
+        "allenai/WildChat-4.8M",
+    ]:
+        dataset = load_dataset(dataset_name)
+        dataset = dataset.map(get_first_user_turn)
+
+        dataset = dataset.filter(
+            lambda example: any(
+                term in example["opening_prompt"].split()
+                for term in filter_terms
+            )
+        )
+        print(dataset.shape)
+        pd_dataset = dataset.with_format("pandas")
+        for prompt_option in prompt_options:
+            dataset.map(lambda x: format_prompt(x, prompt_option))
+            answers = [
+                "yes" in answer[0]["generated_text"][-1]["content"].lower()
+                for answer in tqdm(
+                    model(
+                        convos,
+                        batch_size=batch_size,
+                        do_sample=False,
+                        max_new_tokens=20,
+                    ),
+                    total=len(convos),
+                )
+            ]
+            pd_dataset[prompt_option] = answers
+        pd_dataset.to_pickle(
+            f"scratch/vneplen/implicit-personalization-stereotypes-model-responses/{dataset_name.split('/')[1]}.gz"
+        )
