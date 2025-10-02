9cb3b654fb6bb2f4a42e9de21f69f61b1bfb0615
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-10-02 23:53:01 +0200

    Fix imports

--- probing.py
+++ probing.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import pandas as pd
 import pickle
+from sklearn.pipeline import Pipeline
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, train_test_split
