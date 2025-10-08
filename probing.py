09811588be782f252fc2f233f3afb91ea65fecd9
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-10-08 21:21:47 +0200

    Fix syntax

--- probing.py
+++ probing.py
@@ -185,15 +185,14 @@ if __name__ == "__main__":
         + f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz"
     ):
         df = pd.read_pickle(
-            # f"/scratch/vneplen/sociodemographics-interpretability-mitigation/{args.model.split('/')[1]}_{args.agg_method}_representations.gz",
-            f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz",
+            args.folder
+            + f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz",
             compression="gzip",
         )
     else:
         if os.path.isfile(args.folder + "prism_preprocessed.gz"):
             df = pd.read_pickle(
-                # "/scratch/vneplen/sociodemographics-interpretability-mitigation/prism_preprocessed.gz",
-                "prism_preprocessed.gz",
+                args.folder + "prism_preprocessed.gz",
                 compression="gzip",
             )
         else:
@@ -228,10 +227,7 @@ if __name__ == "__main__":
                     )
                 )
 
-            df.to_pickle(
-                # "/scratch/vneplen/sociodemographics-interpretability-mitigation/prism_preprocessed.gz"
-                "prism_preprocessed.gz"
-            )
+            df.to_pickle(args.folder + "prism_preprocessed.gz")
 
         questions = list(
             set(
@@ -253,8 +249,8 @@ if __name__ == "__main__":
             questions=questions if args.add_questions else None,
         )
         df.to_pickle(
-            # f"/scratch/vneplen/sociodemographics-interpretability-mitigation/{args.model.split('/')[1]}_{args.agg_method}_representations.gz"
-            "{args.model.split('/')[1]}_{args.agg_method}_representations.gz"
+            args.folder
+            + f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz"
         )
 
     # demographic_cols = [
