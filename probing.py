4de99d775b56dda0759e4c2929af570cfc0c6edb
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-10-17 17:30:52 +0200

    Add trustpilot data

--- probing.py
+++ probing.py
@@ -91,47 +91,50 @@ def rebalance(df):
     return df
 
 
-def get_repr(df, model, tokenizer, device, agg_method, questions):
-    convos = [
-        [
-            {
-                "role": turn["role"].replace("model", "assistant"),
-                "content": turn["content"],
-            }
-            for turn in convo
-            if turn["role"] == "user" or turn["if_chosen"] == True
+def get_repr(df, dataset, model, tokenizer, device, agg_method, questions):
+    if dataset == "prism":
+        convos = [
+            [
+                {
+                    "role": turn["role"].replace("model", "assistant"),
+                    "content": turn["content"],
+                }
+                for turn in convo
+                if turn["role"] == "user" or turn["if_chosen"] == True
+            ]
+            for convo in df["conversation_history"].tolist()
         ]
-        for convo in df["conversation_history"].tolist()
-    ]
-    for convo in convos:
-        to_remove = []
-        for i in range(len(convo)):
-            if i > 0 and convo[i]["role"] == convo[i - 1]["role"]:
-                to_remove.append(i)
-        to_remove = to_remove[::-1]
-        for idx in to_remove:
-            del convo[idx]
-    if questions:
-        inputs = [
-            tokenizer.apply_chat_template(
-                convo + [{"role": "user", "content": question}],
-                tokenize=True,
-                add_generation_prompt=True,
-                return_tensors="pt",
-            )
-            for question in questions
-            for convo in convos
-        ]
-    else:
-        inputs = [
-            tokenizer.apply_chat_template(
-                convo,
-                tokenize=True,
-                add_generation_prompt=True,
-                return_tensors="pt",
-            )
-            for convo in convos
-        ]
+        for convo in convos:
+            to_remove = []
+            for i in range(len(convo)):
+                if i > 0 and convo[i]["role"] == convo[i - 1]["role"]:
+                    to_remove.append(i)
+            to_remove = to_remove[::-1]
+            for idx in to_remove:
+                del convo[idx]
+        if questions:
+            inputs = [
+                tokenizer.apply_chat_template(
+                    convo + [{"role": "user", "content": question}],
+                    tokenize=True,
+                    add_generation_prompt=True,
+                    return_tensors="pt",
+                )
+                for question in questions
+                for convo in convos
+            ]
+        else:
+            inputs = [
+                tokenizer.apply_chat_template(
+                    convo,
+                    tokenize=True,
+                    add_generation_prompt=True,
+                    return_tensors="pt",
+                )
+                for convo in convos
+            ]
+    elif dataset == "trustpilot":
+        inputs = tokenizer(df["text"].tolist(), return_tensors="pt")
     if agg_method == "last":
         representations = [
             [
@@ -168,6 +171,7 @@ def get_repr(df, model, tokenizer, device, agg_method, questions):
 
 def train_probe(
     df,
+    dataset,
     n_layers,
     results_file,
     demographic_cols,
@@ -184,36 +188,65 @@ def train_probe(
         "roc_auc_ovr_weighted": "roc_auc_ovr_weighted",
     }
     for demographic_col in tqdm(demographic_cols):
+        if demographic_col == "age" and dataset == "trustpilot":
+            df = df[(df["age"] < 35) | (df["age"] > 45)]
+            df["age"] = np.where(df["age"] < 35, "young", "old")
         if rebalance:
             rebalance_df = balanced_subsample(df, demographic_col)
         results[demographic_col] = []
         for l in tqdm(range(n_layers)):
-            if rebalance:
-                X = np.array(
+            if dataset == "prism":
+                if rebalance:
+                    X = np.array(
+                        [
+                            rep[l]
+                            for rep in rebalance_df["representations"].tolist()
+                        ]
+                    )
+                    y = np.array(rebalance_df[demographic_col].tolist())
+                else:
+                    X = np.array(
+                        [rep[l] for rep in df["representations"].tolist()]
+                    )
+                    y = np.array(df[demographic_col].tolist())
+                keep_idx = np.where(y != "Prefer not to say")[0]
+                y = y[keep_idx]
+                X = X[keep_idx]
+                demo_scoring = {
+                    f"f1_{group}": make_scorer(
+                        f1_score,
+                        average="weighted",
+                        labels=[group],
+                        pos_label=None,
+                    )
+                    for group in np.unique(y)
+                }
+                demo_scoring.update(standard_scoring)
+
+            elif dataset == "trustpilot":
+                X_train = np.array(
                     [
                         rep[l]
-                        for rep in rebalance_df["representations"].tolist()
+                        for rep in df[df["label"] == "train"][
+                            "representations"
+                        ].tolist()
                     ]
                 )
-                y = np.array(rebalance_df[demographic_col].tolist())
-            else:
-                X = np.array(
-                    [rep[l] for rep in df["representations"].tolist()]
+                y_train = np.array(
+                    dfdf[df["label"] == "train"][demographic_col].tolist()
                 )
-                y = np.array(df[demographic_col].tolist())
-            keep_idx = np.where(y != "Prefer not to say")[0]
-            y = y[keep_idx]
-            X = X[keep_idx]
-            demo_scoring = {
-                f"f1_{group}": make_scorer(
-                    f1_score,
-                    average="weighted",
-                    labels=[group],
-                    pos_label=None,
+                X_test = np.array(
+                    [
+                        rep[l]
+                        for rep in df[df["label"] == "test"][
+                            "representations"
+                        ].tolist()
+                    ]
                 )
-                for group in np.unique(y)
-            }
-            demo_scoring.update(standard_scoring)
+                y_test = np.array(
+                    dfdf[df["label"] == "test"][demographic_col].tolist()
+                )
+
             clf = LogisticRegression(
                 random_state=42, class_weight="balanced" if balanced else None
             )
@@ -225,19 +258,27 @@ def train_probe(
                 ) as outfile:
                     pickle.dump(clf, outfile)
             else:
-                scores = cross_validate(
-                    clf,
-                    X,
-                    y,
-                    cv=5,
-                    scoring=demo_scoring,
-                )
-                results[demographic_col].append(
-                    {
-                        metric: scores[f"test_{metric}"]
-                        for metric in demo_scoring
-                    }
-                )
+                if dataset == "prism":
+                    scores = cross_validate(
+                        clf,
+                        X,
+                        y,
+                        cv=5,
+                        scoring=demo_scoring,
+                    )
+                    results[demographic_col].append(
+                        {
+                            metric: scores[f"test_{metric}"]
+                            for metric in demo_scoring
+                        }
+                    )
+                elif dataset == "trustpilot":
+                    clf = clf.fit(X_train, y_train)
+                    y_pred = clf.predict(X_test)
+                    results[demographic_col].append(
+                        {"f1": f1_score(y_test, y_pred)}
+                    )
+
         if not save:
             with open(
                 results_file,
@@ -254,6 +295,13 @@ if __name__ == "__main__":
         type=str,
         default="meta-llama/Llama-3.1-8B-Instruct",
         help="Model to evaluate",
+    )
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        default="prism",
+        help="Dataset to evaluate on",
     )
     parser.add_argument(
         "-am",
@@ -300,52 +348,74 @@ if __name__ == "__main__":
 
         if os.path.isfile(
             args.folder
-            + f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz"
+            + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}_{args.agg_method}_representations.gz"
         ):
             df = pd.read_pickle(
                 args.folder
-                + f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz",
+                + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}_{args.agg_method}_representations.gz",
                 compression="gzip",
             )
         else:
-            if os.path.isfile(args.folder + "prism_preprocessed.gz"):
-                df = pd.read_pickle(
-                    args.folder + "prism_preprocessed.gz",
-                    compression="gzip",
-                )
-            else:
-                conversations = load_dataset(
-                    "HannahRoseKirk/prism-alignment", "conversations"
-                )["train"].to_pandas()
-                survey = load_dataset(
-                    "HannahRoseKirk/prism-alignment", "survey"
-                )["train"].to_pandas()
-
-                df = pd.merge(
-                    conversations,
-                    survey,
-                    on=["user_id"],
-                )
-                to_simplify = ["religion", "ethnicity"]
-                for column in to_simplify:
-                    df[column] = df[column].apply(
-                        lambda x: (
-                            dict(x)["simplified"]
-                            if type(x) == dict
-                            else "Prefer not to say"
-                        )
+            if args.dataset == "prism":
+                if os.path.isfile(args.folder + "prism_preprocessed.gz"):
+                    df = pd.read_pickle(
+                        args.folder + "prism_preprocessed.gz",
+                        compression="gzip",
                     )
-                regions = ["birth_region", "reside_region"]
-                for region in regions:
-                    df[region] = df["location"].apply(
-                        lambda x: (
-                            dict(x)[region]
-                            if type(x) == dict
-                            else "Prefer not to say"
-                        )
-                    )
+                else:
+                    conversations = load_dataset(
+                        "HannahRoseKirk/prism-alignment", "conversations"
+                    )["train"].to_pandas()
+                    survey = load_dataset(
+                        "HannahRoseKirk/prism-alignment", "survey"
+                    )["train"].to_pandas()
 
-                df.to_pickle(args.folder + "prism_preprocessed.gz")
+                    df = pd.merge(
+                        conversations,
+                        survey,
+                        on=["user_id"],
+                    )
+                    to_simplify = ["religion", "ethnicity"]
+                    for column in to_simplify:
+                        df[column] = df[column].apply(
+                            lambda x: (
+                                dict(x)["simplified"]
+                                if type(x) == dict
+                                else "Prefer not to say"
+                            )
+                        )
+                    regions = ["birth_region", "reside_region"]
+                    for region in regions:
+                        df[region] = df["location"].apply(
+                            lambda x: (
+                                dict(x)[region]
+                                if type(x) == dict
+                                else "Prefer not to say"
+                            )
+                        )
+
+                    df.to_pickle(args.folder + "prism_preprocessed.gz")
+
+            elif args.dataset == "trustpilot":
+                df = pd.concat(
+                    [
+                        pd.read_excel("data/en_us_TRAIN.xlsx"),
+                        pd.read_excel("data/en_us_TEST.xlsx"),
+                    ]
+                ).drop(columns=["Unnamed: 0", "age_cat"])
+            df = get_repr(
+                df,
+                dataset,
+                model,
+                tokenizer,
+                device,
+                args.agg_method,
+                questions=questions if args.add_questions else None,
+            )
+            df.to_pickle(
+                args.folder
+                + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}_{args.agg_method}_representations.gz"
+            )
 
             # questions = list(
             #     set(
@@ -358,37 +428,27 @@ if __name__ == "__main__":
             #     )
             # )
 
-            df = get_repr(
-                df,
-                model,
-                tokenizer,
-                device,
-                args.agg_method,
-                questions=questions if args.add_questions else None,
-            )
-            df.to_pickle(
-                args.folder
-                + f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz"
-            )
-
     df = pd.read_pickle(
         args.folder
-        + f"{args.model.split('/')[1]}_{args.agg_method}_representations.gz",
+        + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}_{args.agg_method}_representations.gz",
         compression="gzip",
     )
 
-    demographic_cols = [
-        "age",
-        "gender",
-        "religion",
-        "ethnicity",
-        "employment_status",
-        "education",
-        "birth_region",
-        "reside_region",
-        "marital_status",  # not in facts paper
-        "english_proficiency",  # not in facts paper
-    ]
+    if args.dataset == "prism":
+        demographic_cols = [
+            "age",
+            "gender",
+            "religion",
+            "ethnicity",
+            "employment_status",
+            "education",
+            "birth_region",
+            "reside_region",
+            "marital_status",  # not in facts paper
+            "english_proficiency",  # not in facts paper
+        ]
+    elif args.dataset == "trustpilot":
+        demographic_cols = ["gender", "age"]  # make sure age is second
     n_layers = len(df.iloc[0]["representations"])
 
     if args.rebalance:
@@ -397,27 +457,29 @@ if __name__ == "__main__":
     if args.mode == "probe_cv":
         train_probe(
             df,
+            dataset,
             n_layers,
             args.folder
-            + f"{args.model.split('/')[1]}_probe_results_{args.agg_method}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}.pkl",
+            + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}_probe_results_{args.agg_method}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}.pkl",
             demographic_cols,
             save=False,
             balanced=args.balanced,
             rebalance=args.rebalance,
             save_file=args.folder
-            + f"{args.model.split('/')[1]}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}_probe",
+            + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}_probe",
         )
 
     elif args.mode == "probe_save":
         train_probe(
             df,
+            dataset,
             n_layers,
             args.folder
-            + f"{args.model.split('/')[1]}_probe_results_{args.agg_method}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}.pkl",
+            + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}_probe_results_{args.agg_method}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}.pkl",
             demographic_cols,
             save=True,
             balanced=args.balanced,
             rebalance=args.rebalance,
             save_file=args.folder
-            + f"{args.model.split('/')[1]}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}_probe",
+            + f"{args.model.split('/')[1]}{'_'+args.dataset if args.dataset != 'prism' else ''}{'_balanced' if args.balanced else ''}{'_rebalance' if args.rebalance else ''}_probe",
         )
