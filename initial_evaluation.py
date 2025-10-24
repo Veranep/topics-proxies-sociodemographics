ae248ac893add6543f61da81e1d8b830587769c1
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-10-24 16:41:09 +0200

    Add mitigation

--- initial_evaluation.py
+++ initial_evaluation.py
@@ -315,5 +315,5 @@ if __name__ == "__main__":
     df["answer"] = answers
 
     df.to_pickle(
-        f"/scratch/vneplen/sociodemographics-interpretability-mitigation/{args.model.split('/')[1]}_answers_{args.dataset}.gz"
+        f"/scratch/vneplen/sociodemographics-interpretability-mitigation/{args.model.split('/')[1]}_answers_{args.dataset}{'_' + args.mitigation if args.mitigation else ''}.gz"
     )
