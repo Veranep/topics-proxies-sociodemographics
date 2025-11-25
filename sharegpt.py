3d5059797cb1980827c77d03a38b9f13198f6c29
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-11-25 15:57:30 +0100

    Load train

--- sharegpt.py
+++ sharegpt.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 
 if __name__ == "__main__":
-    dataset = load_dataset("shachardon/ShareLM")
+    dataset = load_dataset("shachardon/ShareLM")["train"]
     print(dataset.features)
     dataset = dataset.flatten()
     print(dataset.features)
