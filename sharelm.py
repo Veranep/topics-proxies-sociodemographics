1bcfe63e995a101c53c38950eee3fa62a2a4237a
Vera Neplenbroek <veraneplenbroek@icloud.com>
2025-11-25 17:08:47 +0100

    Check data

--- sharelm.py
+++ sharelm.py
@@ -2,6 +2,10 @@ from datasets import load_dataset
 
 if __name__ == "__main__":
     dataset = load_dataset("shachardon/ShareLM")["train"]
-    print(dataset.features)
     dataset = dataset.flatten()
-    print(dataset.features)
+    print(len(dataset))
+    for metadata in ["location", "age", "gender"]:
+        filtered = dataset.filter(
+            lambda x: x[f"user_metadata.{metadata}"] != ""
+        )
+        print(len(filtered))
