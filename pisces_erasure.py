bb28bedfa649de0f365bde7469e5df4bcf6226ab
Vera Neplenbroek <veraneplenbroek@icloud.com>
2026-02-27 16:26:56 +0100

    Add erasure code

---
+++ pisces_erasure.py
@@ -0,0 +1,120 @@
+import argparse
+from data_selection_erasure import (
+    neurons_prism,
+    neurons_cad,
+    evaluation_prism,
+    evaluation_cad,
+)
+
+from editor import Concept, unlearn_concept
+from evals import TransformerLensModel
+import numpy as np
+import pandas as pd
+import pickle
+import torch
+from tqdm import tqdm
+from transformer_lens import HookedTransformer
+from transformers import AutoTokenizer
+
+from preprocess_data import get_prism_convos, get_cad_convos, get_chen_convos
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        default="meta-llama/Llama-3.1-8B-Instruct",
+        help="Model to evaluate",
+    )
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        default="prism",
+        help="Dataset to evaluate on",
+    )
+    parser.add_argument(
+        "-df",
+        "--data_folder",
+        type=str,
+        default="",
+    )
+    parser.add_argument(
+        "-rf",
+        "--results_folder",
+        type=str,
+        default="",  # "/scratch/vneplen/sociodemographics-interpretability-mitigation"
+    )
+    parser.add_argument(
+        "-token",
+        type=str,
+        default="",
+        help="Huggingface token that grants access to Llama model",
+    )
+    args = parser.parse_args()
+    if args.token:
+        login(args.token)
+    np.random.seed(42)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tokenizer = AutoTokenizer.from_pretrained(args.model, padding_side="left")
+
+    if not tokenizer.pad_token_id:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    df = pd.read_pickle(f"{args.data_folder}/{args.dataset}_preprocessed.gz")
+
+    if args.dataset == "prism":
+        convo_func = get_prism_convos
+        neurons = neurons_prism
+        evaluation = evaluation_prism
+    elif "cad" in args.dataset:
+        convo_func = get_cad_convos
+        neurons = neurons_cad
+        evaluation = evaluation_cad
+
+    convos = convo_func(df)
+    df_questions = pd.read_pickle(f"{args.data_folder}/questions.gz")
+    df_questions = df_questions["q_id"].isin([f"q_{i}" for i in range(50)])
+    for item in evaluation:
+        eval_convos = [
+            c_id
+            for c_id in evaluation[item][group]
+            for group in evaluation[item]
+        ]
+        evaluation_df = df.loc[df["conversation_id"].isin(eval_convos)]
+        for c in neurons[item]:
+            features = neurons[item][c]
+            concept = Concept(name=c, k=0.4, value=36, features=features)
+            model = HookedTransformer.from_pretrained(
+                args.model, device=device
+            )
+            tm = TransformerLensModel(model)
+            result_df = evaluation_df
+
+            with unlearn_concept(model, concept):
+                for row in tqdm(df_questions.itertuples(index=False)):
+                    convos_and_questions = [
+                        tokenizer.apply_chat_template(
+                            convo
+                            + [{"role": "user", "content": row.question}],
+                            tokenize=False,
+                            add_generation_prompt=True,
+                        )
+                        for convo in convos
+                    ]
+                    tokens = 1
+                    outputs = tm.generate_multiple(
+                        convos_and_questions,
+                        batch_size=8,
+                        max_new_tokens=tokens,
+                        do_sample=False,
+                    )
+
+                    result_df = pd.concat(
+                        [result_df, pd.DataFrame({row.q_id: outputs})], axis=1
+                    )
+                    result_df.to_pickle(
+                        f"{args.results_folder}/{args.model.split('/')[1]}_{args.dataset}_pisces_{item}_{c}_answers.gz"
+                    )
