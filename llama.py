0ada686508872b1a12509cef8addc3c20c975d5b
Vera Neplenbroek <veraneplenbroek@icloud.com>
2026-03-26 19:38:24 +0100

    Fix colons

--- llama.py
+++ llama.py
@@ -125,9 +125,8 @@ def scrub_llama(
         if z_column is not None:
             zs.append(F.one_hot(batch[z_column], num_classes=k))
 
-    print(lr_xs, len(lr_xs), lr_zs[:: len(lr_xs) // 2])
     real_lr = LogisticRegression(max_iter=1000).fit(
-        lr_xs[:: len(lr_xs) // 2], lr_zs[:: len(lr_xs) // 2]
+        lr_xs[: len(lr_xs) // 2], lr_zs[: len(lr_xs) // 2]
     )
     beta = torch.from_numpy(real_lr.coef_)
     print(beta.norm(p=torch.inf))
