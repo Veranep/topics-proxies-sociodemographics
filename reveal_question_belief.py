5588c275b326700e0741053294868d43348caa5e
Vera Neplenbroek <veraneplenbroek@icloud.com>
2026-04-02 23:26:28 +0200

    Fix typo

--- reveal_question_belief.py
+++ reveal_question_belief.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
         ~df_questions["q_id"].isin(df.columns.values)
     ].reset_index(drop=True)
 
-    print("n questions": df_questions.shape[0])
+    print("n questions", df_questions.shape[0])
 
     ########
     for row in tqdm(df_questions.itertuples(index=False)):
